cbb560bd2dad96bb92ed924dcf28a5e74b8abbf3
Mohammed Essa <infernoacm@gmail.com>
2024-03-13 22:09:50 +0300

    Add Theme Files

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('pages.dashboard');
+})->name('home');
+Route::get('/sign-in-static', function () {
+    return view('pages.sign-in-static');
+})->name('sign-in-static');
+
+Route::get('/profile-static', function () {
+    return view('pages.profile-static');
+})->name('profile-static');
+Route::get('/sign-up-static', function () {
+    return view('pages.sign-up-static');
+})->name('sign-up-static');
+Route::get('/profile', function () {
+    return view('pages.profile');
+})->name('profile');
+Route::get('/page', function () {
+    return view('pages.page');
+})->name('page');
+Route::get('/virtual-reality', function () {
+    return view('pages.virtual-reality');
+})->name('virtual-reality');
+Route::get('/rtl', function () {
+    return view('pages.rtl');
+})->name('rtl');
