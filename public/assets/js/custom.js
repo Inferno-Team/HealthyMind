ba507eb28348ce3e9c88ad3c224ea9e659cf8b74
Inferno-Team <36175428+Inferno-Team@users.noreply.github.com>
2024-03-18 20:56:08 +0300

    Update custom.js

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -16,7 +16,7 @@
 
     if (document.getElementsByClassName('navbar-collapse')[0]) {
       var fixedplugin = document.querySelector('.navbar:not(.navbar-expand-lg) .navbar-collapse');
-      var ps2 = new PerfectScrollbar(fixedplugin);
+      // var ps2 = new PerfectScrollbar(fixedplugin);
     };
 
     if (document.getElementsByClassName('fixed-plugin')[0]) {
