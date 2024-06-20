f10b8f774ce44ab774858218160f2e4378d3e678
Mohammed Essa <infernoacm@gmail.com>
2024-06-20 17:46:10 +0300

    removing google map

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -282,8 +282,6 @@
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
     <script src="{{ asset('landing/js/vendor/bootstrap.min.js') }}"></script>
-    <script type="text/javascript"
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
     <script src="{{ asset('landing/js/easing.min.js') }}"></script>
     <script src="{{ asset('landing/js/hoverIntent.js') }}"></script>
     <script src="{{ asset('landing/js/superfish.min.js') }}"></script>
